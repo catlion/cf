0dd43a38248661a895e59782364400b397af35d2
catlion <artem@catlion.name>
2012-03-02 23:46:58 +0400

    a152 almost done

--- a152.hs
+++ a152.hs
@@ -1,11 +1,23 @@
 import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Char
 
-readi' :: String -> Int
-readi' s = read s :: Int
+poss :: [Int] -> [Int]
+poss [] = []
+poss lst = elemIndices max lst
+  where max = head . map $ maximum lst
 
+calc :: [String] -> Int
+calc strs =
+  let ints = [ map digitToInt str | str <- strs]
+      maxposs = map poss ints in
+  map (length . nub . (++)) maxposs
+  
 main = do
   nm <- fmap words getLine
-  let nmi = map readi' nm
-      studs <- mapM
-  --mapM_ print nmi
-  return ()
+  let nmi = map (\x -> read x :: Int) nm
+      n = nmi !! 0
+      --m = nmi !! 1
+  slist <- replicateM n getLine
+  mapM print . show . calc $ slist
