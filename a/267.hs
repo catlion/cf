767e1c2d27f0e14d11ff0d9381324fd1a78b49a8
catlion <ak@ak0.org>
2013-02-13 22:40:10 +0400

    A267 complete

--- a/267.hs
+++ a/267.hs
@@ -1,10 +1,25 @@
+-- http://codeforces.ru/problemset/problem/267/A
+import Data.List
+import Control.Monad
 -- helpers
 getInt = \x -> read x :: Int
-getPair str = (getInt $ lst !! 0, getInt $ lst !! 1) where lst = sort . words str
+getPair str = (getInt $ lst !! 0, getInt $ lst !! 1) where lst = words str
+
+solve :: (Int,Int) -> Int
+solve (_,0) = 0
+solve (0,_) = 0
+solve (x,y) =
+  acc' + (solve (mn,rm))
+  where
+    (mn, mx) = (min x y, max x y)
+    (acc', rm) = mx `divMod` mn
 
 main = do
   count <- fmap getInt getLine
-  pairs <- replicateM count getPair
+  pairs <- replicateM count getLine
+  let ps = map getPair pairs
+      ss = map solve ps
+  mapM_ putStrLn $ map show ss
 {-
 Заданы два числа. До тех пор, пока оба они больше нуля, с ними производят одну и ту же операцию: из большего числа вычитают меньшее. Если числа равны, то из одного вычитают другое. Например, из пары (4,17) за одну операцию получается пара (4,13), а из пары (5,5) пара (0,5).
 
